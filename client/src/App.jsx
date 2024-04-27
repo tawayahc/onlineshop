034e04dde46129bced9cf84f1adcccb88f11558c
Sarun <srnkhumthai@gmail.com>
2024-04-27 15:17:35 +0700

    Update setup client sccessfully

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+    <h1 className='text-3xl underline font-noto'>Hello สวัสดี</h1>
+      <button class="btn btn-primary">Button</button>
+    </>
+  )
+}
+
+export default App
