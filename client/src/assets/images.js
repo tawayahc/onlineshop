b940d7ca53864f5a1a4913a651b0bcd18e5d10d9
Sarun <srnkhumthai@gmail.com>
2024-04-27 18:53:17 +0700

    Image handle
    add package svgr

---
+++ client/src/assets/images.js
@@ -0,0 +1,23 @@
+// Import PNG images
+import logoBlackPNG from './png/logo-black.png';
+import logoWhitePNG from './png/logo-white.png';
+import logoColorPNG from './png/logo-color.png';
+import logoPNG from './png/logo-no-background.png';
+
+// Import SVG images
+import LogoSVG from './svg/logo-no-background.svg';
+import LogoBlackSVG from './svg/logo-black.svg';
+import LogoWhiteSVG from './svg/logo-white.svg';
+import LogoColorSVG from './svg/logo-color.svg';
+
+// Export all images
+export {
+  logoBlackPNG,
+  logoWhitePNG,
+  logoColorPNG,
+  logoPNG,
+  LogoSVG,
+  LogoBlackSVG,
+  LogoWhiteSVG,
+  LogoColorSVG,
+};
