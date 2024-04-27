fbec532fe0b0ee4041a8d0f4dd151f21d5ffd1cf
Sarun <srnkhumthai@gmail.com>
2024-04-27 17:54:38 +0700

    Navigation ✅
    use react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/HomePage';
+import Products from './pages/ProductPages/ProductListPage';
+import ProductDetails from './pages/ProductPages/ProductDetailPage';
+import Cart from './pages/UserPages/CartPage';
+import Checkout from './pages/UserPages/CheckoutPage';
+// import Login from './pages/AuthPages/LoginPage';
+// import Signup from './pages/AuthPages/SignupPage';
+// import NotFound from './pages/NotFoundPage';
+
 function App() {
 
   return (
     <>
-    <h1 className='text-3xl underline font-noto'>Hello สวัสดี</h1>
-      <button class="btn btn-primary">Button</button>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:productId" element={<ProductDetails />} /> 
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+
+        {/* <Route path="/login" element={<Login />} />  
+        <Route path="/signup" element={<Signup />} />  
+        <Route path="*" element={<NotFound />} />  */}
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
