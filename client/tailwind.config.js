0f826e41a93e5ecb20b4a1493ceff0b6aaee193c
Sarun <srnkhumthai@gmail.com>
2024-04-29 01:23:10 +0700

    Prepare package
    - Add react-icon
    - Add auto-fit ( for quality of life )
    - Add fake data

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,41 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
     fontFamily: {
-      noto : ['Noto Sans Thai', 'sans-serif']
-    }
+      noto: ["Noto Sans Thai", "sans-serif"],
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    require("@shrutibalasa/tailwind-grid-auto-fit"),
+  ],
   daisyui: {
     themes: [
-      {'light': {
-        "primary": "#F4F3F0",
-        "primary-focus": "#570df8",
-        "primary-content": "#ffffff",
-        "secondary": "#F4F3F0",
-        "secondary-focus": "#bd0091",
-        "secondary-content": "#ffffff",
-        "accent": "#37cdbe",
-        "accent-focus": "#2aa79b",
-        "accent-content": "#ffffff",
-        "neutral": "#2a2e37",
-        "neutral-focus": "#16181d",
-        "neutral-content": "#ffffff",
-        "base-100": "#3d4451",
-        "base-200": "#2a2e37",
-        "base-300": "#16181d",
-        "base-content": "#ebecf0",
-        "info": "#66c6ff",
-        "success": "#87d039",
-        "warning": "#e2d562",
-        "error": "#ff6f6f"
-      }},
-    ]
-}
-}
-
+      {
+        light: {
+          primary: "#F4F3F0",
+          "primary-focus": "#570df8",
+          "primary-content": "#ffffff",
+          secondary: "#F4F3F0",
+          "secondary-focus": "#bd0091",
+          "secondary-content": "#ffffff",
+          accent: "#37cdbe",
+          "accent-focus": "#2aa79b",
+          "accent-content": "#ffffff",
+          neutral: "#2a2e37",
+          "neutral-focus": "#16181d",
+          "neutral-content": "#ffffff",
+          "base-100": "#3d4451",
+          "base-200": "#2a2e37",
+          "base-300": "#16181d",
+          "base-content": "#ebecf0",
+          info: "#66c6ff",
+          success: "#87d039",
+          warning: "#e2d562",
+          error: "#ff6f6f",
+        },
+      },
+    ],
+  },
+};
