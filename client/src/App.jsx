503d386ac11c320b1fce9989821568cac5b82f01
tawayahc <yotachayawat@gmail.com>
2024-05-04 21:46:39 +0700

    complete frontrnd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import ProductDetails from './pages/ProductPages/ProductDetailPage';
 import Cart from './pages/UserPages/CartPage';
 import Checkout from './pages/UserPages/CheckoutPage';
 import Login from './pages/Login';
+import Register from './pages/Register';
 // import Login from './pages/AuthPages/LoginPage';
 // import Signup from './pages/AuthPages/SignupPage';
 // import NotFound from './pages/NotFoundPage';
@@ -21,9 +22,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/login" element={<Login />} />  
-          <Route path="/signup" element={<Signup />} />  
-          <Route path="*" element={<NotFound />} />  */}
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
