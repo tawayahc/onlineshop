76db8aa541769e374346e9925dcdb273bb52b027
Sarun <srnkhumthai@gmail.com>
2024-05-04 19:21:54 +0700

    Update ProductCard.jsx
    add Wish button

--- client/src/components/Product/ProductCard.jsx
+++ client/src/components/Product/ProductCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BsBagFill, BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 
 function ProductCard({ id, img, title, star, reviews, price }) {
+  // TODO : Get data from database
   const renderStars = (rating) => {
     const fullStars = Math.floor(rating);
     const hasHalfStar = rating % 1 !== 0;
@@ -23,14 +24,31 @@ function ProductCard({ id, img, title, star, reviews, price }) {
     );
   };
   return (
-    // TODO : Wishlist
     // TODO : Add to cart
     // TODO : `/product/${id}`
-    <a href='#'>
+    <a href="#" className="group">
       <div className="card card-compact w-60 min-96 shadow-xl">
         <figure className="h-40">
           <img src={img} alt={title} className="w-full h-full object-cover" />
         </figure>
+        <button className="btn btn-circle btn-sm absolute top-2 right-2">
+          <svg
+            className="h-8 w-8"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z"
+              stroke="#FFFFFF"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
         <div className="card-body flex flex-col">
           <div className="h-12">
             <h3 className="card-title line-clamp-2">{title}</h3>
