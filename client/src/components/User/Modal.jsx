be7a7d69d63d53b9053a35c96529d5b22dd53338
Sarun <srnkhumthai@gmail.com>
2024-05-11 23:25:40 +0700

    Address Modal UI✅
    Need Redux for global data
    ⚠️Can't commit checkout because error

--- client/src/components/User/Modal.jsx
+++ client/src/components/User/Modal.jsx
@@ -5,8 +5,8 @@ const Modal = ({ isOpen, onClose, children, submit }) => {
   if (!isOpen) return null;
 
   return (
-    <dialog className="modal modal-open">
-      <div className="modal-box">
+    <dialog className="modal modal-open min-w-[512px]">
+      <div className="modal-box min-w-fit max-h-[90vh]">
         {children}
         <div className="modal-action m-4">
           <button className="btn" onClick={onClose}>
