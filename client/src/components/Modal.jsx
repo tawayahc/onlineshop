93137ffa0d6c6db09b2e679b819b2fa849e66107
Sarun <srnkhumthai@gmail.com>
2024-05-12 20:52:21 +0700

    Update modal
    Actually I move modal out of user folder

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,22 +1,20 @@
 // Modal.js
-import React from "react";
+import React from 'react';
 
 const Modal = ({ isOpen, onClose, children, submit }) => {
   if (!isOpen) return null;
 
   return (
-    <dialog className="modal modal-open">
-      <div className="modal-box">
+    <dialog className="modal modal-open min-w-[512px]">
+      <div className="modal-box min-w-fit max-h-[90vh]">
         {children}
         <div className="modal-action m-4">
-          <form method="dialog">
-            <button className="btn" onClick={onClose}>
-              ยกเลิก
-            </button>
-            <button className="btn btn-accent" onClick={submit}>
-              บันทึก
-            </button>
-          </form>
+          <button className="btn" onClick={onClose}>
+            ยกเลิก
+          </button>
+          <button className="btn btn-accent" onClick={submit}>
+            บันทึก
+          </button>
         </div>
       </div>
     </dialog>
