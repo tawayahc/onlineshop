87201bace4b04d9e14809f05dc875b0a74b353d8
FlukeSupakorn <supakorn.tungpa@gmail.com>
2024-05-26 08:16:39 +0700

    back to onlineshop

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'onlineshop2',
+  database: 'onlineshop',
 });
 
 module.exports = connection;
