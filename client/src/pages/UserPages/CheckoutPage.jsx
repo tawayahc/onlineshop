76e3420317b92ec92e3ac1e20c25590ffa906be3
Sarun <srnkhumthai@gmail.com>
2024-05-26 19:02:16 +0700

    order & checkout

--- client/src/pages/UserPages/CheckoutPage.jsx
+++ client/src/pages/UserPages/CheckoutPage.jsx
@@ -5,121 +5,252 @@ import PaymentList from "../../components/User/PaymentMethod/PaymentList";
 import NewPaymentModal from "../../components/User/PaymentMethod/NewPaymentModal";
 import AddressList from "../../components/User/Address/AddressList";
 import NewAddressModal from "../../components/User/Address/NewAddressModal";
+import { useRecoilState, useResetRecoilState, useRecoilValue } from "recoil";
+import {
+  addAddressModalState,
+  editAddressModalState,
+  addressFormDataState,
+  addressListState,
+  addPaymentModalState,
+  editPaymentModalState,
+  paymentFormDataState,
+  paymentListState,
+} from "../../recoil/userInfo";
+import {
+  cartState,
+  totalPriceState,
+  totalQuantityState,
+} from "../../recoil/cart";
+import usePayMethodActions from "../../API/userPayMethodActions";
+import { useAddressActions } from "../../API/userAddressActions";
+import useCartActions from "../../API/userCartActions";
+import useOrderActions from "../../API/userOrderActions";
 
 function CheckoutPage() {
-  const [userId, setUserId] = useState(localStorage.getItem("userId"));
+  const userId = localStorage.getItem("userId");
   const [loading, setLoading] = useState(true);
-  const [shippingAddress, setShippingAddress] = useState(null);
-  const [paymentMethod, setPaymentMethod] = useState(null);
-  const handleLinkClick = () => {
-    console.log("Submit button clicked");
-  };
-  const handleSubmitAddress = (event) => {
-    // event.preventDefault();
-    // const formData = new FormData(event.currentTarget);
-    // const data = Object.fromEntries(formData.entries());
-    // console.log("Form Data:", data);
-    // Perform other actions with the data (optional)
-    // setShowNewAddress(false)
-    // console.log(event);
-  };
+  const [selectedAddress, setSelectedAddress] = useState(null);
+  const [selectedPayment, setSelectedPayment] = useState(null);
+  const [showAddAddressModal, setShowAddAddressModal] =
+    useRecoilState(addAddressModalState);
+  const [showEditAddressModal, setShowEditAddressModal] = useRecoilState(
+    editAddressModalState
+  );
+  const [addressFormData, setAddressFormData] =
+    useRecoilState(addressFormDataState);
+  const resetFormDataAddress = useResetRecoilState(addressFormDataState);
+  const [showAddPaymentModal, setShowAddPaymentModal] =
+    useRecoilState(addPaymentModalState);
+  const [showEditPaymentModal, setShowEditPaymentModal] = useRecoilState(
+    editPaymentModalState
+  );
+  const [paymentFormData, setPaymentFormData] =
+    useRecoilState(paymentFormDataState);
+  const resetFormDataPayment = useResetRecoilState(paymentFormDataState);
+
+  const { fetchPaymentMethod, addPaymentMethod, updatePaymentMethod } =
+    usePayMethodActions(userId);
+  const paymentList = useRecoilValue(paymentListState);
+
+  const { fetchShippingAddress, updateShippingAddress, addShippingAddress } =
+    useAddressActions(userId);
+  const addressList = useRecoilValue(addressListState);
+
+  const { fetchCart } = useCartActions(userId);
+  const [cart, setCart] = useRecoilState(cartState);
+  const [totalPrice, setTotalPrice] = useRecoilState(totalPriceState);
+  const [totalQuantity, setTotalQuantity] = useRecoilState(totalQuantityState);
+
   useEffect(() => {
-    const fetchUserInfo = async () => {
-      if (!userId) return;
+    setLoading(true);
+    fetchPaymentMethod().finally(() => setLoading(false));
+    setLoading(true);
+    fetchShippingAddress().finally(() => setLoading(false));
+    setLoading(true);
+    fetchCart().finally(() => setLoading(false));
+  }, []);
 
-      try {
-        setLoading(true);
-        const shippingaddressResponse = await axios.get(
-          `http://localhost:3333/shippingaddress/${userId}`
-        );
-        const paymentmethodResponse = await axios.get(
-          `http://localhost:3333/paymentmethod/${userId}`
-        );
-        if (
-          shippingaddressResponse.data.status === "ok" &&
-          paymentmethodResponse.data.status === "ok"
-        ) {
-          setShippingAddress(shippingaddressResponse.data.data);
-          setPaymentMethod(paymentmethodResponse.data.data);
-        }
-      } catch (error) {
-        console.error("Error:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const displayAddress = (address) => (
+    <p>
+      {address.FullName}, {address.PhoneNumber}, {address.Address},{" "}
+      {address.SubDistrict}, {address.District}, {address.Province},{" "}
+      {address.PostalCode}
+    </p>
+  );
 
-    fetchUserInfo();
-  }, [userId]);
   const [showPaymentList, setShowPaymentList] = useState(false);
   const [showNewPayment, setShowNewPayment] = useState(false);
   const [showAddressList, setShowAddressList] = useState(false);
   const [showNewAddress, setShowNewAddress] = useState(false);
 
-  const openPaymentList = () => {
-    setShowPaymentList(true);
+  const { processOrder } = useOrderActions(userId);
+  const handleSubmitOrder = async () => {
+    const orderData = {
+      userId,
+      selectedAddress,
+      selectedPayment,
+      cart,
+      totalPrice,
+      totalQuantity,
+      status: "Processing",
+      expectedDate: new Date().toISOString().split("T")[0]
+    };
+    console.log(orderData);
+    setLoading(true);
+    processOrder(orderData).finally(() => setLoading(false));
   };
 
-  const closePaymentList = () => {
-    setShowPaymentList(false);
+  const openAddAddressModal = () => {
+    resetFormDataAddress();
+    setShowAddAddressModal(true);
   };
 
-  const openNewPayment = () => {
-    setShowNewPayment(true);
+  const closeAddAddressModal = () => {
+    setShowAddAddressModal(false);
   };
 
-  const closeNewPayment = () => {
-    setShowNewPayment(false);
+  const openEditAddressModal = (addressId) => {
+    const addressToEdit = addressList.find(
+      (address) => address.ShippingAddressID === addressId
+    );
+    if (addressToEdit) {
+      setAddressFormData(addressToEdit);
+      setShowEditAddressModal(true);
+    }
+  };
+
+  const closeEditAddressModal = () => {
+    setShowEditAddressModal(false);
   };
 
   const openAddressList = () => {
     setShowAddressList(true);
   };
-
   const closeAddressList = () => {
     setShowAddressList(false);
   };
 
-  const openNewAddress = () => {
-    setShowNewAddress(true);
+  const handleSubmitAddress = (formData) => {
+    const isEdit = !!formData.ShippingAddressID;
+    const action = isEdit ? updateShippingAddress : addShippingAddress;
+
+    if (typeof action === "function") {
+      action(formData).finally(() => {
+        if (isEdit) {
+          closeEditAddressModal();
+        } else {
+          closeAddAddressModal();
+        }
+        fetchShippingAddress();
+      });
+    } else {
+      console.error("Error: action is not a function");
+    }
   };
 
-  const closeNewAddress = () => {
-    setShowNewAddress(false);
+  const openAddPaymentModal = () => {
+    resetFormDataPayment();
+    setShowAddPaymentModal(true);
+  };
+  const closeAddPaymentModal = () => {
+    setShowAddPaymentModal(false);
+  };
+
+  const openEditPaymentModal = (paymentId) => {
+    const paymentToEdit = paymentList.find(
+      (payment) => payment.PaymentMethodID === paymentId
+    );
+    if (paymentToEdit) {
+      setPaymentFormData(paymentToEdit);
+      setShowEditPaymentModal(true);
+    }
+  };
+
+  const closeEditPaymentModal = () => {
+    setShowEditPaymentModal(false);
+  };
+
+  const openPaymentList = () => {
+    setShowPaymentList(true);
+  };
+  const closePaymentList = () => {
+    setShowPaymentList(false);
+  };
+
+  const handleSubmitPayment = (formData) => {
+    const isEdit = !!formData.PaymentMethodID;
+    const action = isEdit ? updatePaymentMethod : addPaymentMethod;
+
+    if (typeof action === "function") {
+      action(formData).finally(() => {
+        if (isEdit) {
+          closeEditPaymentModal();
+        } else {
+          closeAddPaymentModal();
+        }
+        setLoading(true);
+        fetchPaymentMethod().finally(() => setLoading(false));
+      });
+    } else {
+      console.error("action is not a function");
+    }
   };
 
   return (
     <div className="font-noto">
       <div className="flex flex-row justify-between mx-48">
         <div className="flex flex-col gap-4">
-          <div
-            className="btn btn-outline btn-accent w-[450px] 2xl:w-[800px] h-24 no-animation "
-            onClick={openNewAddress}
-          >
-            เพิ่มที่อยู่จัดส่ง
-          </div>
-          <div className="card w-[450px] 2xl:w-[800px] bg-primary text-primary-content card-bordered">
-            <div className="card-body">
-              <h2 className="card-title ">ที่อยู่จัดส่ง</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn" onClick={openAddressList}>
-                  เปลี่ยน
-                </button>
+          {addressList && addressList.length > 0 ? (
+            <div className="card w-[450px] 2xl:w-[800px] bg-primary text-primary-content card-bordered">
+              <div className="card-body">
+                <h2 className="card-title">ที่อยู่จัดส่ง</h2>
+                <div>
+                  <div>
+                    {selectedAddress
+                      ? displayAddress(selectedAddress)
+                      : "เลือกที่อยู่จัดส่ง"}
+                  </div>
+                </div>
+                <div className="card-actions justify-end">
+                  <button className="btn" onClick={openAddressList}>
+                    เปลี่ยน
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="card w-[450px] 2xl:w-[800px] bg-primary text-primary-content card-bordered">
-            <div className="card-body">
-              <h2 className="card-title">ช่องทางการชําระเงิน</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn" onClick={openPaymentList}>
-                  เปลี่ยน
-                </button>
+          ) : (
+            <div
+              className="btn btn-outline btn-accent w-[450px] 2xl:w-[800px] h-24 no-animation "
+              onClick={openAddAddressModal}
+            >
+              เพิ่มที่อยู่จัดส่ง
+            </div>
+          )}
+
+          {paymentList && paymentList.length > 0 ? (
+            <div className="card w-[450px] 2xl:w-[800px] bg-primary text-primary-content card-bordered">
+              <div className="card-body">
+                <h2 className="card-title">ช่องทางการชําระเงิน</h2>
+                <p>
+                  {selectedPayment
+                    ? "xxxx xxxx xxxx " + selectedPayment.CardNumber
+                    : "เลือกช่องทางการชําระเงิน"}
+                </p>
+                <div className="card-actions justify-end">
+                  <button className="btn" onClick={openPaymentList}>
+                    เปลี่ยน
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
+          ) : (
+            <div
+              className="btn btn-outline btn-accent w-[450px] 2xl:w-[800px] h-24 no-animation "
+              onClick={openAddPaymentModal}
+            >
+              เพิ่มช่องทางการชําระเงิน
+            </div>
+          )}
         </div>
         <div>
           <div className="card w-96 bg-base-100 shadow-xl">
@@ -127,7 +258,7 @@ function CheckoutPage() {
               <h2 className="card-title text-2xl mb-2">สรุปการสั่งซื้อ</h2>
               <div className="flex flex-row justify-between">
                 <p>ยอดรวมสินค้า</p>
-                <p className="font-bold text-right">ราคาสินค้า</p>
+                <p className="font-bold text-right">฿{totalPrice}</p>
               </div>
               <div className="flex flex-row justify-between">
                 <p>ค่าจัดส่ง</p>
@@ -136,10 +267,13 @@ function CheckoutPage() {
               <div className="divider my-2"></div>
               <div className="flex flex-row justify-between mb-4">
                 <h1 className="text-xl">ยอดสุทธิ</h1>
-                <h1 className="text-xl font-bold text-right">ราคาสินค้า</h1>
+                <h1 className="text-xl font-bold text-right">฿{totalPrice}</h1>
               </div>
               <div className="card-actions justify-center ">
-                <button className="btn btn-wide btn-error">
+                <button
+                  className="btn btn-wide btn-error"
+                  onClick={handleSubmitOrder}
+                >
                   ทำการสั่งซื้อ
                 </button>
               </div>
@@ -148,36 +282,52 @@ function CheckoutPage() {
         </div>
       </div>
 
-      <Modal
-        isOpen={showPaymentList}
-        onClose={closePaymentList}
-        submit={handleLinkClick}
-      >
-        <PaymentList onClick={openNewPayment} />
+      {/* Payment */}
+      <Modal isOpen={showPaymentList} onClose={closePaymentList}>
+        <PaymentList
+          onClick={openAddPaymentModal}
+          onSelect={setSelectedPayment}
+        />
       </Modal>
 
       <Modal
-        isOpen={showNewPayment}
-        onClose={closeNewPayment}
-        submit={handleLinkClick}
+        isOpen={showAddPaymentModal}
+        onClose={closeAddPaymentModal}
+        onSubmit={() => handleSubmitPayment(paymentFormData)}
       >
         <NewPaymentModal />
       </Modal>
 
       <Modal
-        isOpen={showAddressList}
-        onClose={closeAddressList}
-        submit={handleLinkClick}
+        isOpen={showEditPaymentModal}
+        onClose={closeEditPaymentModal}
+        onSubmit={() => handleSubmitPayment(paymentFormData)}
       >
-        <AddressList onClick={handleLinkClick} />
+        <NewPaymentModal />
+      </Modal>
+
+      {/* Address */}
+      <Modal isOpen={showAddressList} onClose={closeAddressList}>
+        <AddressList
+          onClick={openAddAddressModal}
+          onSelect={setSelectedAddress}
+        />
       </Modal>
 
       <Modal
-        isOpen={showNewAddress}
-        onClose={closeNewAddress}
-        submit={handleLinkClick}
+        isOpen={showAddAddressModal}
+        onClose={closeAddAddressModal}
+        onSubmit={() => handleSubmitAddress(addressFormData)}
       >
-        <NewAddressModal handleSubmit={handleLinkClick} />
+        <NewAddressModal />
+      </Modal>
+
+      <Modal
+        isOpen={showEditAddressModal}
+        onClose={closeEditAddressModal}
+        onSubmit={() => handleSubmitAddress(addressFormData)}
+      >
+        <NewAddressModal />
       </Modal>
     </div>
   );
